64c303369c05a7656ddabb40bbf9c4a13e89134f
Ntokozo-skosana <2440810@students.wits.ac.za>
2025-09-03 19:25:09 +0200

    React + TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1 style={{ fontSize: "2rem", color: "blue" }}>Community Connect</h1>
+      <p style={{ marginTop: "1rem", color: "gray" }}>
+        React + TypeScript is working!
+      </p>
+    </div>
+  );
+}
+
+export default App;
